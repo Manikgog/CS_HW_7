d529f7c897889644af131149e414096c1ba8f836
Manikgog <m4xin4t4@gmail.com>
2023-10-15 22:24:29 +0500

    Add project files.

---
+++ Money.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_HW_7
+{
+    internal class Money
+    {
+        private bool plus;
+        private long rub;
+        private byte kop;
+
+        private void Normalize(long kop)    // метод для нормализации величины суммы копеек при передаче суммы большей чем 99 копеек
+        {
+            if(kop >= 0)
+            {
+                this.plus = true;
+            }
+            else
+            {
+                this.plus = false;
+            }
+            kop = Math.Abs(kop);
+            if (kop >= 100)
+            {
+                this.rub += kop/100;
+                this.kop += (byte)(kop%100);
+                if (this.kop >= 100)
+                {
+                    this.rub += this.kop/100;
+                    this.kop = (byte)(this.kop%100);
+                }
+            }
+            else
+            {
+                this.kop += (byte)kop;
+                if (this.kop >= 100)
+                {
+                    this.rub += this.kop/100;
+                    this.kop = (byte)(this.kop%100);
+                }
+            }
+        }
+
+        public Money()
+        {
+            this.plus = true;
+            this.rub = 0;
+            this.kop = 0;
+        }
+
+
+
+        public Money(long rub, byte kop)
+        {
+            try
+            {
+                if(rub < 0)
+                {
+                    throw new Exception("Банкрот");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            if (rub < 0)
+            {
+                Normalize(-kop + rub * 100);
+            }
+            else
+            {
+                Normalize(kop + rub * 100);
+            }
+        }
+
+        public Money(long rub, int kop)
+        {
+            try
+            {
+                if (rub < 0)
+                {
+                    throw new Exception("Банкрот");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+            if (rub < 0)
+            {
+                Normalize(-kop + rub * 100);
+            }
+            else
+            {
+                Normalize(kop + rub * 100);
+            }
+        }
+
+        public Money(long kop)
+        {
+            try
+            {
+                if (kop < 0)
+                {
+                    throw new Exception("Банкрот");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            Normalize(kop);
+        }
+
+        public long Rub
+        {
+            get => this.rub;
+            set
+            {
+                this.rub = value;
+            }
+        }
+
+        public byte Kop
+        {
+            get => this.kop;
+            set
+            {
+                Normalize(value);
+            }
+        }
+
+        public override string ToString()
+        {
+            if (plus == true)
+            {
+                return this.rub + " р. " + this.kop + " коп.";
+            }
+            return " - " + this.rub + " р. " + this.kop + " коп.";
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public static Money operator +(Money m1, Money m2)
+        {
+            long kop1;
+            long kop2;
+            if (m1.plus == false) 
+            {
+                kop1 = -1 * (m1.kop + m1.rub * 100);
+            }
+            else
+            {
+                kop1 = m1.kop + m1.rub * 100;
+            }
+
+            if (m2.plus == false)
+            {
+                kop2 = -1 * (m2.kop + m2.rub * 100);
+            }
+            else
+            {
+                kop2 = m2.kop + m2.rub * 100;
+            }
+
+
+            Money result;
+            
+            result = new Money(kop1 + kop2);
+            
+            return result;
+        }
+
+        public static Money operator -(Money m1, Money m2)
+        {
+            long kop1;
+            long kop2;
+            if (m1.plus == false)
+            {
+                kop1 = -1 * (m1.kop + m1.rub * 100);
+            }
+            else
+            {
+                kop1 = m1.kop + m1.rub * 100;
+            }
+
+            if (m2.plus == false)
+            {
+                kop2 = -1 * (m2.kop + m2.rub * 100);
+            }
+            else
+            {
+                kop2 = m2.kop + m2.rub * 100;
+            }
+
+            Money result;
+
+            result = new Money(kop1 - kop2);
+
+            return result;
+        }
+
+        public static Money operator *(Money m1, int multiplier)
+        {
+            long kop1;
+            if (m1.plus == false)
+            {
+                kop1 = -1 * (m1.kop + m1.rub * 100);
+            }
+            else
+            {
+                kop1 = m1.kop + m1.rub * 100;
+            }
+
+            Money result = new Money(kop1 * multiplier);
+
+            return result;
+           
+        }
+
+        public static Money operator /(Money m1, int divider)
+        {
+            long kop_;
+            if (m1.plus == false)
+            {
+                kop_ = -1 * (m1.kop + m1.rub * 100);
+            }
+            else
+            {
+                kop_ = m1.kop + m1.rub * 100;
+            }
+            try
+            {
+                if ((int)(m1.rub * 100 + m1.kop)%divider == 0)
+                {
+                    long Kop = kop_/divider;
+                    Money result = new Money(Kop);
+                    return result;
+                }
+                else
+                {
+                    Console.WriteLine(m1.ToString() + " не делится нацело на " + divider + ".");
+                    return null;
+                }
+            }catch(DivideByZeroException dbz)
+            {
+                Console.WriteLine(dbz.Message);
+            }
+            return null;
+        }
+
+        public static Money operator ++(Money m)
+        {
+            long kop_;
+            if (m.plus == false)
+            {
+                kop_ = -1 * (m.kop + m.rub * 100);
+            }
+            else
+            {
+                kop_ = m.kop + m.rub * 100;
+            }
+            kop_++;
+            Money result = new Money(kop_);
+            return result;
+        }
+
+        public static Money operator --(Money m)
+        {
+            long kop_;
+            if (m.plus == false)
+            {
+                kop_ = -1 * (m.kop + m.rub * 100);
+            }
+            else
+            {
+                kop_ = m.kop + m.rub * 100;
+            }
+            kop_--;
+            Money result = new Money(kop_);
+            return result;
+        }
+
+        public static bool operator ==(Money m1, Money m2)
+        {
+            return m1.rub == m2.rub && m1.kop == m2.kop && m1.plus == m2.plus;
+        }
+
+        public static bool operator !=(Money m1, Money m2)
+        {
+            return m1.rub != m2.rub || m1.kop != m2.kop || m1.plus != m2.plus;
+        }
+
+        public static bool operator >(Money m1, Money m2)
+        {
+            long kop1;
+            long kop2;
+            if (m1.plus == false)
+            {
+                kop1 = -1 * (m1.kop + m1.rub * 100);
+            }
+            else
+            {
+                kop1 = m1.kop + m1.rub * 100;
+            }
+
+            if (m2.plus == false)
+            {
+                kop2 = -1 * (m2.kop + m2.rub * 100);
+            }
+            else
+            {
+                kop2 = m2.kop + m2.rub * 100;
+            }
+
+            return kop1 > kop2;
+        }
+
+        public static bool operator <(Money m1, Money m2)
+        {
+            long kop1;
+            long kop2;
+            if (m1.plus == false)
+            {
+                kop1 = -1 * (m1.kop + m1.rub * 100);
+            }
+            else
+            {
+                kop1 = m1.kop + m1.rub * 100;
+            }
+
+            if (m2.plus == false)
+            {
+                kop2 = -1 * (m2.kop + m2.rub * 100);
+            }
+            else
+            {
+                kop2 = m2.kop + m2.rub * 100;
+            }
+
+            return kop1 < kop2;
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_HW_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task_1();
+        }
+
+        public static void Task_1()
+        {
+            /*Написать класс Money, предназначенный для хранения денежной суммы (в гривнах и копейках). Для класса
+        реализовать перегрузку операторов + (сложение денежных сумм), – (вычитание сумм), / (деление суммы на целое
+        число), * (умножение суммы на целое число), ++ (сумма
+        увеличивается на 1 копейку), -- (сумма уменьшается на
+        1 копейку), <, >, ==, !=.
+        Класс не может содержать отрицательную сумму.
+        В случае если при исполнении какой-либо операции получается отрицательная сумма денег, то класс генерирует
+        исключительную ситуацию «Банкрот».
+        Программа должна с помощью меню продемонстрировать все возможности класса Money. Обработка исключительных ситуаций производится в программе.*/
+            int rub = 0;
+            byte kop = 0;
+            byte choice = 0;
+            bool allRight = true;
+
+            Money money = EnteringOfMoney();
+
+            bool exit = false;
+
+            do
+            {
+
+                Console.Clear();
+                do
+                {
+                    Console.WriteLine("Перечень действий: ");
+                    Console.WriteLine("1. Сложение денежных сумм.");
+                    Console.WriteLine("2. Вычитание денежных сумм.");
+                    Console.WriteLine("3. Деление суммы счёта на целое число.");
+                    Console.WriteLine("4. Умножение суммы счёта на целое число.");
+                    Console.WriteLine("5. Увеличение суммы счёта на одну копейку.");
+                    Console.WriteLine("6. Уменьшение суммы счёта на одну копейку.");
+                    Console.WriteLine("7. Сравнение суммы счёта с введённой суммой.");
+                    Console.WriteLine("8. Выход.");
+                    try
+                    {
+                        Console.Write("Введите номер действия: ");
+                        choice = Convert.ToByte(Console.ReadLine());
+                        if (choice < 1 || choice > 8)
+                        {
+                            throw new ArgumentOutOfRangeException("Число должно быть от 1 до 7 включительно.");
+                        }
+                    }
+                    catch (FormatException fe)
+                    {
+                        allRight = false;
+                        Console.WriteLine(fe.Message);
+                        Console.ReadKey();
+                    }
+                    catch (Exception ex)
+                    {
+                        allRight = false;
+                        Console.WriteLine(ex.Message);
+                        Console.ReadKey();
+                    }
+                    Console.Clear();
+                    if (allRight)
+                    {
+                        break;
+                    }
+                    allRight = true;
+                } while (true);
+
+                if(choice == 1)     // сложение денежных сумм
+                {
+                    Console.WriteLine("Сложение денежных сумм.");
+                    Money money_2 = EnteringOfMoney();
+
+                    Console.WriteLine(money.ToString() + " + " + money_2.ToString() + " = " + (money + money_2));
+                    money = money + money_2;
+                    Console.ReadKey();
+                }else if (choice == 2)  // вычитание дележных сумм
+                {
+                    Console.WriteLine("Вычитание денежных сумм.");
+                    Money money_2 = EnteringOfMoney();
+                    Console.WriteLine(money.ToString() + " - " + money_2.ToString() + " = " + (money - money_2));
+                    money = money - money_2;
+                    Console.ReadKey();
+                }
+                else if(choice == 3)    // Деление суммы счёта на целое число
+                {
+                    Console.WriteLine("Деление суммы счёта на целое число.");
+                    int number = CheckNumber();                    
+                    Console.WriteLine(money.ToString() + " / " + number + " = " + (money / number));
+                    money = money / number;
+                    Console.ReadKey();
+                }
+                else if (choice == 4)   // Умножение суммы счёта на целое число
+                {
+                    Console.WriteLine("Умножение суммы счёта на целое число.");
+                    int number = CheckNumber();
+                    Console.WriteLine(money.ToString() + " * " + number + " = " + (money * number));
+                    money = money * number;
+                    Console.ReadKey();
+                }
+                else if (choice == 5)   // Увеличение суммы счёта на одну копейку
+                {
+                    Console.WriteLine("Увеличение суммы счёта на одну копейку.");
+                    Console.Write(money.ToString() + "++ = "); 
+                    money++;
+                    Console.Write(money);
+                    Console.ReadKey();
+                }
+                else if (choice == 6)   // Уменьшение суммы счёта на одну копейку
+                {
+                    Console.WriteLine("Уменьшение суммы счёта на одну копейку.");
+                    Console.Write(money.ToString() + "-- = ");
+                    money--;
+                    Console.Write(money);
+                    Console.ReadKey();
+                }
+                else if (choice == 7)   // Сравнение суммы счёта с введённой суммой
+                {
+                    Console.WriteLine("Сравнение суммы счёта с введённой суммой.");
+                    Money money_2 = EnteringOfMoney();
+                    if(money_2 < money)
+                    {
+                        Console.WriteLine(money_2.ToString() + " < " + money.ToString());
+                    }else if (money < money_2)
+                    {
+                        Console.WriteLine(money_2.ToString() + " > " + money.ToString());
+                    }
+                    else if (money == money_2)
+                    {
+                        Console.WriteLine(money.ToString() + " = " + money_2.ToString());
+                    }
+                    Console.ReadKey();
+                }
+                else if (choice == 8)   // Выход
+                {
+                    exit = true;
+                }
+
+
+
+            } while (!exit);
+        }
+
+        public static Money EnteringOfMoney()
+        {
+            int rub = 0;
+            int kop = 0;
+            bool allRight = true;
+            do
+            {
+                Console.Write("Введите размер счёта:\nколичество рублей: ");
+                try
+                {
+                    rub = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("количество копеек: ");
+                    kop = Convert.ToByte(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    allRight = false;
+                    Console.WriteLine(ex.Message);
+                    Console.ReadKey();
+                }
+                if (allRight)
+                {
+                    break;
+                }
+                allRight = true;
+                Console.Clear();
+            } while (true);
+            Money money = new Money(rub, kop);
+            return money;
+        }
+
+        public static int CheckNumber()
+        {
+            int number = 0;
+            bool flag;
+            do
+            {
+                flag = true;
+                try
+                {
+                    Console.Write("Введите целое число: ");
+                    number = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    flag = false;
+                }
+
+
+            } while (!flag);
+            return number;
+        }
+    }
+}
